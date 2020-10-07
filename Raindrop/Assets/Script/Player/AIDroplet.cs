3654ce8954a079cbca30a9cfd4a9aa11fb58367e
Tacey127 <jtacey@hotmail.co.uk>
2020-10-07 16:52:07 +0100

    Input Overhaul

---
+++ Raindrop/Assets/Script/Player/AIDroplet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDroplet : DropletBase
+{
+    //takes size, speed and direction
+    AIDroplet(float size, float speed, float direction)
+    {
+
+    }
+}
