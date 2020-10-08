fe3312f617ba69ab8db2ee7b56ead6af5892a47c
Tacey127 <jtacey@hotmail.co.uk>
2020-10-08 17:53:51 +0100

    collision added

---
+++ Raindrop/Assets/Script/Droplet/DropletBase.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropletBase : MonoBehaviour
+{
+	[SerializeField] protected GameObject spriteHolder;
+	[SerializeField] float speed = 1;
+
+	//z axis rotation
+	float rotation = 0;
+
+	protected Vector3 direction = Vector3.zero;
+	protected Vector3 position = Vector3.zero;
+
+    #region base
+
+	// DO NOT OVERRIDE UPDATE
+	void Update()
+	{
+		RunUpdate();
+	}
+
+    #endregion
+
+    protected virtual void RunUpdate()
+    {
+		UpdatePosition();
+    }
+
+	protected void UpdatePosition()
+    {
+		transform.Translate(nextPosition(direction));
+	}
+
+	Vector3 nextPosition(Vector3 direction)
+    {
+		return direction * speed * Time.deltaTime;
+	}
+
+    #region collision
+
+    private void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("TR");
+
+
+		DropletBase otherDroplet = other.GetComponent<DropletBase>();
+
+
+		if (transform.localScale.x > other.transform.localScale.x)
+		{
+			OnAbsorb(otherDroplet);
+		}
+
+		if (transform.localScale.x < other.transform.localScale.x)
+		{
+			OnAbsorbed(otherDroplet);
+		}
+
+	}
+
+	protected void OnAbsorb(DropletBase other)
+	{
+		transform.localScale += other.transform.localScale * 0.2f;
+		Destroy(other.gameObject);
+	}
+
+	protected void OnAbsorbed(DropletBase other)
+    {
+
+    }
+
+    #endregion
+}
+
---
+++ Raindrop/Assets/Script/Droplet/Player/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : DropletBase
+{
+    #region singleton
+
+    public PlayerController instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    #endregion
+
+    [SerializeField] float turnSmoothing = 0.125f;
+ 
+    protected override void RunUpdate()
+    {
+        AddInput();
+        RotateSprite(direction.x);
+        base.RunUpdate();
+    }
+
+    void AddInput()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+        direction.Normalize();
+    }
+
+    void RotateSprite(float dir)
+    {
+        Quaternion desiredRotation = Quaternion.identity;
+        if(dir != 0)//input accounts for deadzone
+        {
+            desiredRotation.z = Mathf.Deg2Rad * 15 * dir;
+        }
+        else
+        {
+            desiredRotation.z = 0;
+        }
+
+        Quaternion smoothedRotation = Quaternion.Lerp(spriteHolder.transform.rotation, desiredRotation, turnSmoothing * Time.deltaTime);
+        spriteHolder.transform.rotation = smoothedRotation;
+    }
+
+}
+
